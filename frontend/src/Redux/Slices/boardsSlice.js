24f2acebee63b2a1ea03fed980daa3d3791f8d79
Lokeshkale2000 <lokesh.21910292@viit.ac.in>
2024-10-22 00:12:12 +0530

    first commit

---
+++ frontend/src/Redux/Slices/boardsSlice.js
@@ -0,0 +1,61 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  boardsData: {},
+  pending: true,
+  backgroundImages: [
+    "https://images.unsplash.com/photo-1485356824219-4bc17c2a2ea7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dHJlbGxvfGVufDB8fDB8fHww",
+    "https://images.unsplash.com/photo-1486728297118-82a07bc48a28?q=80&w=1858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1692338763339-8d616483cd08?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1686642848560-e980295a9360?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fHRyZWxsb3xlbnwwfHwwfHx8MA%3D%3D",
+    "https://images.unsplash.com/photo-1686767113769-8e3f2793e111?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1484448295605-a7dc0e347ab4?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1667899358047-f2458452158c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://plus.unsplash.com/premium_photo-1670060949801-481ee30a9f7b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1511268594014-0e9d3ea5c33e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ],
+  smallPostfix:
+    "?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw3MDY2fDB8MXxjb2xsZWN0aW9ufDJ8MzE3MDk5fHx8fHwyfHwxNjM2NjUzNDgz&ixlib=rb-1.2.1&q=80&w=400",
+  creating: false,
+};
+
+const boardsSlice = createSlice({
+  name: "boards",
+  initialState,
+  reducers: {
+    startFetchingBoards: (state) => {
+      state.pending = true;
+    },
+    successFetchingBoards: (state, action) => {
+      state.boardsData = action.payload.boards;
+      state.pending = false;
+    },
+    failFetchingBoards: (state) => {
+      state.pending = false;
+    },
+    startCreatingBoard: (state) => {
+      state.creating = true;
+    },
+    successCreatingBoard: (state, action) => {
+      state.boardsData.push(action.payload);
+      state.creating = false;
+    },
+    failCreatingBoard: (state) => {
+      state.creating = true;
+    },
+    reset:(state)=>{
+      state=initialState;
+    }
+  },
+});
+
+export const {
+  startFetchingBoards,
+  successFetchingBoards,
+  failFetchingBoards,
+  startCreatingBoard,
+  successCreatingBoard,
+  failCreatingBoard,
+  reset
+} = boardsSlice.actions;
+export default boardsSlice.reducer;
